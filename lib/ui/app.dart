4030b97156d2632ef5ba094fbe69274c8bc392ef
Danilo Donado <djosedonado@unicesar.edu.co>
2023-03-17 17:19:13 -0500

    se agrega libreria getmateriapp

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -4,13 +4,14 @@ import 'package:clase_estados/ui/pages/carrito/add.dart';
 import 'package:clase_estados/ui/pages/carrito/list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "Articulos",
       theme: ThemeData.dark(),
       home: ListaArticulos(),
